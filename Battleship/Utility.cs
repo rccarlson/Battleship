034dc385f59d8df9f7eaef3fb85d466d8478a57e
Riley Carlson <rileyccarlson97@gmail.com>
2023-02-08 18:55:02 -0500

    Convert namespace layout

--- Battleship/Utility.cs
+++ Battleship/Utility.cs
@@ -5,35 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Battleship
+namespace Battleship;
+
+internal static class Utility
 {
-	internal static class Utility
+	private static readonly Random Random = new();
+
+	/// <summary>Fisher-Yates shuffle</summary>
+	public static IList<T> Shuffle<T>(this IList<T> list)
 	{
-		private static readonly Random Random = new();
-
-		/// <summary>Fisher-Yates shuffle</summary>
-		public static IList<T> Shuffle<T>(this IList<T> list)
+		if (list is null) return list;
+		int n = list.Count;
+		while (n > 1)
 		{
-			if (list is null) return list;
-			int n = list.Count;
-			while (n > 1)
-			{
-				n--;
-				int k = Random.Next(n + 1);
-				T value = list[k];
-				list[k] = list[n];
-				list[n] = value;
-			}
-			return list;
+			n--;
+			int k = Random.Next(n + 1);
+			T value = list[k];
+			list[k] = list[n];
+			list[n] = value;
 		}
-
-		public static TimeSpan Profile(Action action)
-		{
-			var sw = Stopwatch.StartNew();
-			action();
-			sw.Stop();
-			return sw.Elapsed;
-		}
-		public static double ProfileMs(Action action) => Profile(action).TotalMilliseconds;
+		return list;
 	}
+
+	public static TimeSpan Profile(Action action)
+	{
+		var sw = Stopwatch.StartNew();
+		action();
+		sw.Stop();
+		return sw.Elapsed;
+	}
+	public static double ProfileMs(Action action) => Profile(action).TotalMilliseconds;
 }
