0e1f5f689380afa2ebc830c8d76a4c12e2ce2e9f
Riley Carlson <riley.carlson@ksb.com>
2023-02-09 13:16:04 -0500

    Remove unneeded Point struct & add hash code override

--- Battleship/Point.cs
+++ Battleship/Point.cs
@@ -6,25 +6,10 @@ using System.Threading.Tasks;
 
 namespace Battleship
 {
-	public record struct Point(int X, int Y)
-	{
-		public override string ToString() => $"({X}, {Y})";
-
-		public static explicit operator Point(ReadOnlyPoint p) => new(p.X, p.Y);
-
-		public static bool operator ==(Point a, ReadOnlyPoint b) => a.X == b.X && a.Y == b.Y;
-		public static bool operator !=(Point a, ReadOnlyPoint b) => a.X != b.X || a.Y != b.Y;
-	}
-
-
 	public readonly record struct ReadOnlyPoint(int X, int Y)
 	{
 		public override string ToString() => $"({X}, {Y})";
-
-		public static implicit operator ReadOnlyPoint(Point p) => new(p.X, p.Y);
-
-		public static bool operator ==(ReadOnlyPoint a, Point b) => a.X == b.X && a.Y == b.Y;
-		public static bool operator !=(ReadOnlyPoint a, Point b) => a.X != b.X || a.Y != b.Y;
+		public override int GetHashCode() => HashCode.Combine(X, Y);
 
 	}
 }
