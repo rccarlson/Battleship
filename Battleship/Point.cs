57490a78472010a02526b82319c8a6c723aeadac
Riley Carlson <riley.carlson@ksb.com>
2023-02-10 07:51:43 -0500

    Add equality override to readonlypoint

--- Battleship/Point.cs
+++ Battleship/Point.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Battleship
 {
-	public readonly record struct ReadOnlyPoint(int X, int Y)
+	public readonly record struct ReadOnlyPoint(int X, int Y):IEqualityComparer<ReadOnlyPoint>
 	{
 		public override string ToString() => $"({X}, {Y})";
 		public override int GetHashCode() => HashCode.Combine(X, Y);
 
+		public bool Equals(ReadOnlyPoint x, ReadOnlyPoint y) => x.X == y.X && x.Y == y.Y;
+
+		public int GetHashCode([DisallowNull] ReadOnlyPoint obj) => HashCode.Combine(obj.X, obj.Y);
 	}
 }
