f1211923c69292a29f009c97fa4ddc3bcd609e0e
Riley Carlson <riley.carlson@ksb.com>
2023-02-10 10:26:22 -0500

    Change Battleship program to focus on profiling game speed, rather than simulating a single game

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -1,9 +1,31 @@
-﻿using static Battleship.Utility;
+﻿using System.Diagnostics;
+using static Battleship.Utility;
 namespace Battleship;
 
 internal class Program
 {
 	static void Main(string[] args)
+	{
+		//SimulateGame(true);
+		for (int i = 0; i < 10; i++) SimulateGame(false); // warm caches
+
+		for (int i = 0; i < 1000; i++)
+		{
+			var average = Enumerable.Range(0, 10_000).AsParallel().Average(_ => ProfileMs(() => SimulateGame(false)));
+			Console.WriteLine(average);
+		}
+
+	}
+
+	static double ProfileMs(Action action)
+	{
+		var sw = Stopwatch.StartNew();
+		action();
+		sw.Stop();
+		return sw.Elapsed.TotalMilliseconds;
+	}
+
+	static void SimulateGame(bool printProgress)
 	{
 		var board = new Board(RuleSet.Standard);
 		for (int y = 0; y < board.Rules.BoardHeight; y++)
@@ -11,12 +33,14 @@ internal class Program
 			for (int x = 0; x < board.Rules.BoardWidth; x++)
 			{
 				board.Shoot(x, y);
+				if(printProgress)
+				{
 				Console.Clear();
 				Console.WriteLine(board.ToString());
 				Console.WriteLine(board.IsWon ? "Game won" : "");
-				Thread.Sleep(200);
+					Thread.Sleep(100);
+				}
 			}
 		}
-
 	}
 }
