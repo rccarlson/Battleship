10313f4a8a2a719ab36fa4dab9d61c5916624e47
Riley Carlson <riley.carlson@ksb.com>
2023-02-10 15:28:20 -0500

    Validate boards before running profiling

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -6,15 +6,16 @@ internal class Program
 {
 	static void Main(string[] args)
 	{
-		//SimulateGame(true);
-		for (int i = 0; i < 10; i++) SimulateGame(false); // warm caches
+		SimulateGame(true);
+		for (int i = 0; i < 100; i++) SimulateGame(false).Validate(); // warm caches
 
-		for (int i = 0; i < 1000; i++)
+		for (int i = 0; i < 100; i++) SimulateGame(false); // hook for profiling
+
+		for (int i = 0; i < 100; i++)
 		{
-			var average = Enumerable.Range(0, 10_000).AsParallel().Average(_ => ProfileMs(() => SimulateGame(false)));
+			var average = Enumerable.Range(0, 20_000).AsParallel().Average(_ => ProfileMs(() => SimulateGame(false)));
 			Console.WriteLine(average);
 		}
-
 	}
 
 	static double ProfileMs(Action action)
@@ -25,7 +26,7 @@ internal class Program
 		return sw.Elapsed.TotalMilliseconds;
 	}
 
-	static void SimulateGame(bool printProgress)
+	static Board SimulateGame(bool printProgress)
 	{
 		var board = new Board(RuleSet.Standard);
 		for (int y = 0; y < board.Rules.BoardHeight; y++)
@@ -38,9 +39,10 @@ internal class Program
 					Console.Clear();
 					Console.WriteLine(board.ToString());
 					Console.WriteLine(board.IsWon ? "Game won" : "");
-					Thread.Sleep(100);
+					Thread.Sleep(50);
 				}
 			}
 		}
+		return board;
 	}
 }
